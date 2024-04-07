9df95216972b06975d0158cb99f2be3345364d15
João Paulo Ferreira <paulojp.dev@gmail.com>
2024-04-07 15:48:07 -0300

    feat: create projects

---
+++ stateful-any-api/src/test/java/com/microservice/statefulanyapi/StatefulAnyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.statefulanyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatefulAnyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
